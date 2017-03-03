050cce1050cd7aa8d4aae66d3ba301d81a8be50e
Patrick <patricktrherbert@gmail.com>
2017-03-02 17:27:17 -0800

    Create New Xcode Project for Camera Feature

--- XCode Projects/FamiliarizeMyselfWithXCode/FamiliarizeMyselfWithXCode/ViewController.swift
+++ XCode Projects/FamiliarizeMyselfWithXCode/FamiliarizeMyselfWithXCode/ViewController.swift
@@ -8,10 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        picker.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +20,32 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    //--------------------------
+    //Patrick's Code Starts Here
+    //--------------------------
+    
+    @IBOutlet weak var myImageView: UIView!
+    
+    let picker = UIImagePickerController()
+    
+    @IBAction func photoShoot(_ sender: UIBarButtonItem) {
+    }
+    
+    @IBAction func photoFromLibrary(_ sender: UIBarButtonItem) {
+        picker.allowsEditing = false
+        picker.sourceType = .photoLibrary
+        picker.mediaTypes = UIImagePickerController.availableMediaTypes(for: .photoLibrary)!
+        present(picker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
 
 }
 
